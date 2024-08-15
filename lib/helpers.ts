63f4ee88f2d10aab76e10bde77095bf39856f041
Semih <shouldahjdh@gmail.com>
2024-08-15 03:49:14 +0300

    "Added type annotations for 'a' and 'b' parameters in sortByCreationTime function"

--- lib/helpers.ts
+++ lib/helpers.ts
@@ -1,6 +1,6 @@
 
 export const sortByCreationTime = async ({bookmarks}:{bookmarks:any}) => {
-    return bookmarks?.sort((a, b) => {
+    return bookmarks?.sort((a:any, b:any) => {
         // Assuming creationTime is a number or sortable string
         return b.creationTime - a.creationTime;
       });
